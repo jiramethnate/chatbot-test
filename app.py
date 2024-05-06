e3a118e2070c5e9225de8f7fa94d246c8fa433f4
jiramethnate <135225542+jiramethnate@users.noreply.github.com>
2024-05-06 14:28:45 +0700

    change: line token

--- app.py
+++ app.py
@@ -1,218 +1,270 @@
-#!/usr/bin/python
-#-*-coding: utf-8 -*-
-##from __future__ import absolute_import
-###
-from flask import Flask, jsonify, render_template, request
-import json
-import numpy as np
-import pandas as pd
-import requests
-import geopy.distance as ps
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage, FlexSendMessage
-)
-from linebot.models.template import *
+# -*- coding: utf-8 -*-
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from __future__ import unicode_literals
+
+import errno
+import os
+import sys
+import tempfile
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
 app = Flask(__name__)
 
-lineaccesstoken = 'fCPVtYJvqDDnmA7dyg2FxRKkK3J7iDzljVr+ET/lIU0nlOZa4zY/5Y/Q2RlHIUIA7Vc/pBNdtPUnex1NUPF6sfDLBFOYGsVjZKeAA4IdUfbYCVun8SNyTEVYeup0+1EZdaOtkwr6kQYIB4NvUHrodwdB04t89/1O/w1cDnyilFU='
-line_bot_api = LineBotApi(lineaccesstoken)
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = '8eb22c28c165bc7a8d13996b01767078'
+channel_access_token = 'fCPVtYJvqDDnmA7dyg2FxRKkK3J7iDzljVr+ET/lIU0nlOZa4zY/5Y/Q2RlHIUIA7Vc/pBNdtPUnex1NUPF6sfDLBFOYGsVjZKeAA4IdUfbYCVun8SNyTEVYeup0+1EZdaOtkwr6kQYIB4NvUHrodwdB04t89/1O/w1cDnyilFU='
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-casedata = pd.read_excel('casedata.xlsx')
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
-####################### new ########################
-@app.route('/')
-def index():
-    return "Hello World!"
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 
 
-@app.route('/webhook', methods=['POST'])
+# function for create tmp dir for download content
+def make_static_tmp_dir():
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
+
+@app.route("/callback", methods=['POST'])
 def callback():
-    json_line = request.get_json(force=False,cache=False)
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    no_event = len(decoded['events'])
-    for i in range(no_event):
-        event = decoded['events'][i]
-        event_handle(event)
-    return '',200
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-def event_handle(event):
-    print(event)
+    # handle webhook body
     try:
-        userId = event['source']['userId']
-    except:
-        print('error cannot get userId')
-        return ''
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-    try:
-        rtoken = event['replyToken']
-    except:
-        print('error cannot get rtoken')
-        return ''
-    if 'message' in event.keys():
-        try:
-            msgType = event["message"]["type"]
-            msgId = event["message"]["id"]
-        except:
-            print('error cannot get msgID, and msgType')
-            sk_id = np.random.randint(1,17)
-            replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
-            line_bot_api.reply_message(rtoken, replyObj)
-            return ''
-    if 'postback' in event.keys():
-        msgType = 'postback'
+    return 'OK'
 
-    if msgType == "text":
-        msg = str(event["message"]["text"])
-        replyObj = handle_text("โย่วๆ")
-        line_bot_api.reply_message(rtoken, replyObj)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+    
+    if text == 'Hi':
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='สวัสดีครับ Pensook ยินดีรับใช้'))
 
-    if msgType == "postback":
-        msg = str(event["postback"]["data"])
-        replyObj = handle_postback(msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+    # if text == 'profile':
+    #     if isinstance(event.source, SourceUser):
+    #         profile = line_bot_api.get_profile(event.source.user_id)
+    #         line_bot_api.reply_message(
+    #             event.reply_token, [
+    #                 TextSendMessage(
+    #                     text='Display name: ' + profile.display_name
+    #                 ),
+    #                 TextSendMessage(
+    #                     text='Status message: ' + profile.status_message
+    #                 )
+    #             ]
+    #         )
+    #     else:
+    #         line_bot_api.reply_message(
+    #             event.reply_token,
+    #             TextMessage(text="Bot can't use profile API without user ID"))
+    # elif text == 'bye':
+    #     if isinstance(event.source, SourceGroup):
+    #         line_bot_api.reply_message(
+    #             event.reply_token, TextMessage(text='Leaving group'))
+    #         line_bot_api.leave_group(event.source.group_id)
+    #     elif isinstance(event.source, SourceRoom):
+    #         line_bot_api.reply_message(
+    #             event.reply_token, TextMessage(text='Leaving group'))
+    #         line_bot_api.leave_room(event.source.room_id)
+    #     else:
+    #         line_bot_api.reply_message(
+    #             event.reply_token,
+    #             TextMessage(text="Bot can't leave from 1:1 chat"))
+    # elif text == 'confirm':
+    #     confirm_template = ConfirmTemplate(text='Do it?', actions=[
+    #         MessageTemplateAction(label='Yes', text='Yes!'),
+    #         MessageTemplateAction(label='No', text='No!'),
+    #     ])
+    #     template_message = TemplateSendMessage(
+    #         alt_text='Confirm alt text', template=confirm_template)
+    #     line_bot_api.reply_message(event.reply_token, template_message)
+    # elif text == 'buttons':
+    #     buttons_template = ButtonsTemplate(
+    #         title='My buttons sample', text='Hello, my buttons', actions=[
+    #             URITemplateAction(
+    #                 label='Go to line.me', uri='https://line.me'),
+    #             PostbackTemplateAction(label='ping', data='ping'),
+    #             PostbackTemplateAction(
+    #                 label='ping with text', data='ping',
+    #                 text='ping'),
+    #             MessageTemplateAction(label='Translate Rice', text='米')
+    #         ])
+    #     template_message = TemplateSendMessage(
+    #         alt_text='Buttons alt text', template=buttons_template)
+    #     line_bot_api.reply_message(event.reply_token, template_message)
+    # elif text == 'carousel':
+    #     carousel_template = CarouselTemplate(columns=[
+    #         CarouselColumn(text='hoge1', title='fuga1', actions=[
+    #             URITemplateAction(
+    #                 label='Go to line.me', uri='https://line.me'),
+    #             PostbackTemplateAction(label='ping', data='ping')
+    #         ]),
+    #         CarouselColumn(text='hoge2', title='fuga2', actions=[
+    #             PostbackTemplateAction(
+    #                 label='ping with text', data='ping',
+    #                 text='ping'),
+    #             MessageTemplateAction(label='Translate Rice', text='米')
+    #         ]),
+    #     ])
+    #     template_message = TemplateSendMessage(
+    #         alt_text='Buttons alt text', template=carousel_template)
+    #     line_bot_api.reply_message(event.reply_token, template_message)
+    # elif text == 'imagemap':
+    #     pass
+    # else:
+    #     line_bot_api.reply_message(
+    #         event.reply_token, TextSendMessage(text=event.message.text))
 
-    if msgType == "location":
-        lat = event["message"]["latitude"]
-        lng = event["message"]["longitude"]
-        #txtresult = handle_location(lat,lng,casedata,3)
-        result = getcaseflex(lat,lng)
-        replyObj = FlexSendMessage(alt_text='Flex Message alt text', contents=result)
-        line_bot_api.reply_message(rtoken, replyObj)
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        LocationSendMessage(
+            title=event.message.title, address=event.message.address,
+            latitude=event.message.latitude, longitude=event.message.longitude
+        )
+    )
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
+
+
+# Other Message Type
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
     else:
-        sk_id = np.random.randint(1,17)
-        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
-        line_bot_api.reply_message(rtoken, replyObj)
-    return ''
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save content.'),
+            TextSendMessage(text=request.host_url + os.path.join('static', 'tmp', dist_name))
+        ])
 
 
-dat = pd.read_excel('addb.xlsx')
-def getdata(query):
-    res = dat[dat['QueryWord']==query]
-    if len(res)==0:
-        return 'nodata'
-    else:
-        productName = res['ProductName'].values[0]
-        imgUrl = res['ImgUrl'].values[0]
-        desc = res['Description'].values[0]
-        cont = res['Contact'].values[0]
-        return productName,imgUrl,desc,cont
-
-def flexmessage(query):
-    res = getdata(query)
-    if res == 'nodata':
-        return 'nodata'
-    else:
-        productName,imgUrl,desc,cont = res
-    flex = '''
-    {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "url": "%s",
-          "margin": "none",
-          "size": "full",
-          "aspectRatio": "1:1",
-          "aspectMode": "cover",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
-          }
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "md",
-          "action": {
-            "type": "uri",
-            "label": "Action",
-            "uri": "https://linecorp.com"
-          },
-          "contents": [
-            {
-              "type": "text",
-              "text": "%s",
-              "size": "xl",
-              "weight": "bold"
-            },
-            {
-              "type": "text",
-              "text": "%s",
-              "wrap": true
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "button",
-              "action": {
-                "type": "postback",
-                "label": "ติดต่อคนขาย",
-                "data": "%s"
-              },
-              "color": "#F67878",
-              "style": "primary"
-            }
-          ]
-        }
-      }'''%(imgUrl,productName,desc,cont)
-    return flex
-
-from linebot.models import (TextSendMessage,FlexSendMessage)
-import json
-
-def handle_text(inpmessage):
-    flex = flexmessage(inpmessage)
-    if flex == 'nodata':
-        replyObj = TextSendMessage(text=inpmessage)
-    else:
-        flex = json.loads(flex)
-        replyObj = FlexSendMessage(alt_text='Flex Message alt text', contents=flex)
-    return replyObj
-
-def handle_postback(inpmessage):
-    replyObj = TextSendMessage(text=inpmessage)
-    return replyObj
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text='Got follow event'))
 
 
-def handle_location(lat,lng,cdat,topK):
-    result = getdistace(lat, lng,cdat)
-    result = result.sort_values(by='km')
-    result = result.iloc[0:topK]
-    txtResult = ''
-    for i in range(len(result)):
-        kmdistance = '%.1f'%(result.iloc[i]['km'])
-        newssource = str(result.iloc[i]['News_Soruce'])
-        txtResult = txtResult + 'ห่าง %s กิโลเมตร\n%s\n\n'%(kmdistance,newssource)
-    return txtResult[0:-2]
+@handler.add(UnfollowEvent)
+def handle_unfollow():
+    app.logger.info("Got Unfollow event")
 
 
-def getcaseflex(lat,lng):
-    url = 'http://botnoiflexapi.herokuapp.com/getnearcase?lat=%s&long=%s'%(lat,lng)
-    res = requests.get(url).json()
-    return res
-
-def getdistace(latitude, longitude,cdat):
-  coords_1 = (float(latitude), float(longitude))
-  ## create list of all reference locations from a pandas DataFrame
-  latlngList = cdat[['Latitude','Longitude']].values
-  ## loop and calculate distance in KM using geopy.distance library and append to distance list
-  kmsumList = []
-  for latlng in latlngList:
-    coords_2 = (float(latlng[0]),float(latlng[1]))
-    kmsumList.append(ps.vincenty(coords_1, coords_2).km)
-  cdat['km'] = kmsumList
-  return cdat
+@handler.add(JoinEvent)
+def handle_join(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Joined this ' + event.source.type))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@handler.add(LeaveEvent)
+def handle_leave():
+    app.logger.info("Got leave event")
+
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == 'ping':
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='pong'))
+
+
+@handler.add(BeaconEvent)
+def handle_beacon(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Got beacon event. hwid=' + event.beacon.hwid))
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=5000, type=int, help='port')
+    arg_parser.add_argument('-d', '--debug', action='store_true', help='debug')
+    options = arg_parser.parse_args()
+
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=options.debug, port=options.port)
